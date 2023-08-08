518464e959d4139cbfadcf82aa7d463aa71d47ce
viduycheung <viduy@appest.com>
2023-08-08 13:39:57 +0800

    feat(oauth): ticktick oauth implement

--- main.ts
+++ main.ts
@@ -1,134 +1,232 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	requestUrl,
+} from "obsidian";
+import { BrowserWindow } from "@electron/remote";
 
-// Remember to rename these classes and interfaces!
+const client_id = "rz4MFhTD80QwBsi6bc";
+const client_secret = "t)6H$+3+O+$5BtFRArG44sv*3$qvZ8z)";
 
-interface MyPluginSettings {
-	mySetting: string;
+interface TickTickPluginSettings {
+	token: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: TickTickPluginSettings = {
+	token: "",
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class TickTickPlugin extends Plugin {
+	settings: TickTickPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		statusBarItemEl.setText("Status Bar Text");
 
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: "open-ticktick-oauth-modal",
+			name: "Connect to TickTick",
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				if (this.settings.token) {
+					new Notice("Already sign in TickTick");
+					return;
 				}
-			}
+				new OauthModal(this.app, this).open();
+			},
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new SettingTab(this.app, this));
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
+		console.log(this.settings);
+		this.requestGET("/open/v1/project").then((data) => {
+			if (data) {
+				const { json } = data;
+				console.log(json);
+			}
+		});
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	requestGET = (url: string) => {
+		console.log({
+			Authorization: `Bearer ${this.settings.token}`,
+		});
+		return requestUrl({
+			method: "GET",
+			url: "https://api.ticktick.com" + url,
+			headers: {
+				Authorization: `Bearer ${this.settings.token}`,
+			},
+		}).catch((e) => {
+			console.log(e);
+		});
+	};
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	requestPOST = (url: string, body: Record<string, any>) => {
+		return requestUrl({
+			method: "POST",
+			url: "https://api.ticktick.com" + url,
+			headers: {
+				Authorization: `Bearer ${this.settings.token}`,
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(body),
+		}).catch((e) => {
+			console.log(e);
+		});
+	};
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class OauthModal extends Modal {
+	plugin: TickTickPlugin;
+
+	constructor(app: App, plugin: TickTickPlugin) {
 		super(app);
+		this.plugin = plugin;
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+		new Setting(contentEl).setName(
+			"This plugin need connect to your TickTick Account"
+		);
+		new Setting(contentEl).addButton((btn) =>
+			btn
+				.setButtonText("Sign in TickTick")
+				.setCta()
+				.onClick(() => {
+					const callbackUrl = "http://localhost/callback";
+
+					const window = new BrowserWindow({
+						width: 600,
+						height: 800,
+						webPreferences: {
+							nodeIntegration: false, // We recommend disabling nodeIntegration for security.
+							contextIsolation: true, // We recommend enabling contextIsolation for security.
+							// see https://github.com/electron/electron/blob/master/docs/tutorial/security.md
+						},
+					});
+					const url = `https://ticktick.com/oauth/authorize?client_id=${client_id}&redirect_uri=${encodeURIComponent(
+						callbackUrl
+					)}&response_type=code`;
+
+					window.loadURL(url);
+					console.log({ url });
+					const {
+						session: { webRequest },
+					} = window.webContents;
+
+					const filter = {
+						urls: ["http://localhost/callback*"],
+					};
+
+					webRequest.onBeforeRequest(filter, async ({ url }) => {
+						const urlParams = new URL(url).searchParams;
+						const code = urlParams.get("code");
+						console.log(
+							"user granted previleges to temp credentials (requestToken) " +
+								url,
+							{ code }
+						);
+						if (code) {
+							const urlencoded = new URLSearchParams();
+							urlencoded.append("client_id", client_id);
+							urlencoded.append("client_secret", client_secret);
+							urlencoded.append("code", code);
+							urlencoded.append(
+								"grant_type",
+								"authorization_code"
+							);
+							urlencoded.append(
+								"scope",
+								"tasks:write tasks:read"
+							);
+							urlencoded.append("redirect_uri", callbackUrl);
+							console.log({ urlencoded }, urlencoded.toString());
+							requestUrl({
+								method: "POST",
+								url: "https://ticktick.com/oauth/token",
+								headers: {
+									"Content-Type":
+										"application/x-www-form-urlencoded",
+								},
+								body: urlencoded.toString(),
+							})
+								.then(async ({ json }) => {
+									console.log(json);
+									this.plugin.settings.token =
+										json.access_token;
+									console.log(this.plugin.settings);
+									await this.plugin.saveSettings();
+									window.close();
+									this.close();
+								})
+								.catch((err) => {
+									// error
+									console.log(err);
+									window.close();
+									this.close();
+								});
+						} else {
+							// error
+							window.close();
+							this.close();
+						}
+					});
+				})
+		);
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class SettingTab extends PluginSettingTab {
+	plugin: TickTickPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: TickTickPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("TickTick API token")
+			.setDesc("")
+			.addButton((btn) =>
+				btn
+					.setButtonText("Sign in TickTick")
+					.setCta()
+					.onClick(() => {
+						new OauthModal(this.app, this.plugin).open();
+					})
+			);
 	}
 }
