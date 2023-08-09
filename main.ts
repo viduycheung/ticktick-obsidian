c5fcfadc599c323c686388e2fbcf96269a62c494
viduycheung <viduy@appest.com>
2023-08-09 13:46:45 +0800

    feat(create task): add new create task modal

--- main.ts
+++ main.ts
@@ -6,18 +6,39 @@ import {
 	PluginSettingTab,
 	Setting,
 	requestUrl,
+	DropdownComponent,
+	ButtonComponent,
+	TextComponent,
+	TextAreaComponent,
 } from "obsidian";
 import { BrowserWindow } from "@electron/remote";
 
 const client_id = "rz4MFhTD80QwBsi6bc";
 const client_secret = "t)6H$+3+O+$5BtFRArG44sv*3$qvZ8z)";
 
+type Project = {
+	id: string;
+	name: string;
+	sortOrder: number;
+	isClosed: boolean;
+};
+
+type Task = {
+	title: string;
+	content: string;
+	priority: number;
+	//  0 | 1 | 3 | 5
+	projectId: string;
+};
+
 interface TickTickPluginSettings {
 	token: string;
+	projects: Project[];
 }
 
 const DEFAULT_SETTINGS: TickTickPluginSettings = {
 	token: "",
+	projects: [],
 };
 
 export default class TickTickPlugin extends Plugin {
@@ -31,7 +52,7 @@ export default class TickTickPlugin extends Plugin {
 		statusBarItemEl.setText("Status Bar Text");
 
 		this.addCommand({
-			id: "open-ticktick-oauth-modal",
+			id: "ticktick-open-oauth-modal",
 			name: "Connect to TickTick",
 			callback: () => {
 				if (this.settings.token) {
@@ -42,11 +63,61 @@ export default class TickTickPlugin extends Plugin {
 			},
 		});
 
+		this.addCommand({
+			id: "ticktick-create-task-modal",
+			name: "New Task",
+			callback: () => {
+				if (!this.settings.token) {
+					new Notice("User not sign in");
+					return;
+				}
+				new CreateTaskModal(this.app, this).open();
+			},
+		});
+
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SettingTab(this.app, this));
+
+		this.registerInterval(
+			window.setInterval(() => {
+				if (this.settings.token) {
+					this.fetchProjects();
+				}
+			}, 5 * 60 * 1000)
+		);
 	}
 
 	onunload() {}
+
+	fetchProjects = async () => {
+		try {
+			const data = await this.requestGET("/open/v1/project");
+			if (data) {
+				const { json } = data;
+				if (Array.isArray(json)) {
+					this.settings.projects = json;
+					this.saveSettings();
+				}
+			}
+			return new Promise((resolve, reject) => resolve("ok"));
+		} catch (error) {
+			return new Promise((resolve, reject) => reject(error));
+		}
+	};
+
+	createTask = async (taskData: Task) => {
+		try {
+			const data = await this.requestPOST("/open/v1/task", taskData);
+			if (data) {
+				const { json } = data;
+				console.log(json);
+				new Notice(`Add ${taskData.title}`);
+			}
+		} catch (error) {
+			new Notice("Add Failed");
+			return new Promise((resolve, reject) => reject(error));
+		}
+	};
 
 	async loadSettings() {
 		this.settings = Object.assign(
@@ -55,12 +126,7 @@ export default class TickTickPlugin extends Plugin {
 			await this.loadData()
 		);
 		console.log(this.settings);
-		this.requestGET("/open/v1/project").then((data) => {
-			if (data) {
-				const { json } = data;
-				console.log(json);
-			}
-		});
+		this.fetchProjects();
 	}
 
 	async saveSettings() {
@@ -107,10 +173,8 @@ class OauthModal extends Modal {
 	}
 
 	onOpen() {
-		const { contentEl } = this;
-		new Setting(contentEl).setName(
-			"This plugin need connect to your TickTick Account"
-		);
+		const { contentEl, titleEl } = this;
+		titleEl.setText("This plugin need connect to your TickTick Account");
 		new Setting(contentEl).addButton((btn) =>
 			btn
 				.setButtonText("Sign in TickTick")
@@ -196,6 +260,105 @@ class OauthModal extends Modal {
 					});
 				})
 		);
+	}
+
+	onClose() {
+		const { contentEl } = this;
+		contentEl.empty();
+	}
+}
+
+class CreateTaskModal extends Modal {
+	plugin: TickTickPlugin;
+
+	taskData: Task;
+
+	constructor(app: App, plugin: TickTickPlugin) {
+		super(app);
+		this.plugin = plugin;
+		this.taskData = {
+			title: "",
+			content: "",
+			priority: 0,
+			projectId: "inbox",
+		};
+	}
+
+	onOpen() {
+		const { contentEl, titleEl } = this;
+		titleEl.setText("New Task");
+
+		// title
+		const titleComp = new TextComponent(
+			contentEl.createDiv({ cls: "modal-line" })
+		)
+			.setPlaceholder("Title")
+			.setValue("")
+			.onChange((value) => {
+				this.taskData.title = value;
+			});
+		titleComp.inputEl.style.flex = "auto";
+
+		// content
+		const contentComp = new TextAreaComponent(
+			contentEl.createDiv({ cls: "modal-line" })
+		)
+			.setPlaceholder("Content")
+			.setValue("")
+			.onChange((value) => {
+				this.taskData.content = value;
+			});
+		contentComp.inputEl.style.flex = "auto";
+
+		// list
+		const projectLine = contentEl.createDiv({ cls: "modal-line" });
+		projectLine.createEl("div", { cls: "label", text: "List" });
+		const projectComp = new DropdownComponent(projectLine)
+			.addOptions(
+				this.plugin.settings.projects.reduce(
+					(id2Name: Record<string, string>, project) => {
+						id2Name[project.id] = project.name;
+						return id2Name;
+					},
+					{ inbox: "Inbox" }
+				)
+			)
+			.onChange((value) => {
+				console.log(value);
+				this.taskData.priority = +value;
+			});
+		projectComp.selectEl.style.flex = "auto";
+
+		// priority
+		const priorityLine = contentEl.createDiv({ cls: "modal-line" });
+		priorityLine.createEl("div", { cls: "label", text: "Priority" });
+		const priorityComp = new DropdownComponent(priorityLine)
+			.addOptions({
+				0: "None",
+				1: "Low",
+				3: "Medium",
+				5: "High",
+			})
+			.onChange((value) => {
+				console.log(value);
+				this.taskData.priority = +value;
+			});
+		priorityComp.selectEl.style.flex = "auto";
+
+		// submit
+		const submitComp = new ButtonComponent(contentEl)
+			.setButtonText("Create")
+			.setCta()
+			.onClick(() => {
+				console.log(this.taskData);
+				if (this.taskData.title) {
+					this.plugin.createTask(this.taskData);
+					this.close();
+				} else {
+					new Notice("Task title can't be empty");
+				}
+			});
+		submitComp.buttonEl.style.float = "right";
 	}
 
 	onClose() {
